48094cf8651cffb3a492c0b541f744b1cc853739
simeon <simeonpitts@gmail.com>
2023-01-27 18:22:02 -0800

    I have added a window.onload with let to my index.js it will..

---
+++ js/index.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    const form = document.querySelector ("form");
+    form.onsubmit = function(event) {
+        event.preventDefault();
+        //question 1
+        let learn = document.getElementById("learn");
+        let cross = document.getElementById("cross-platfom");
+        //question 2
+        let angular = document.getElementById("angular");
+        let net = document.getElementById("net");
+        //question 3
+        let online = document.getElementById("online");
+        let inschool = document.getElementById("inschool");
+        //question 4
+        let gaming = document.getElementById("gaming");
+        let web = document.getElementById("web");
+        //question 5
+        let Mcdonald = document.getElementById("Mcdonald");
+        let Burger = document.getElementById("Burger");
+
+        if () {
+            
+        }
+    }
+}
