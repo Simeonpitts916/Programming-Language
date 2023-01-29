de0c0f258f0d1ca621964cac3cc234f016669957
simeon <simeonpitts@gmail.com>
2023-01-28 22:04:09 -0800

    i have added a readme it will..

---
+++ README.md
@@ -0,0 +1,52 @@
+# my programing
+## names of contributors
+simeon pitts
+
+## Description of application
+here you can find out what program works fo you 
+
+## bTechnologies used
+css
+
+html
+
+## Application setup instructions
+I have added a window.onload with let to my index.js it will..
+
+@Simeonpitts916
+Simeonpitts916 committed yesterday
+ 
+Commits on Jan 26, 2023
+I have added an answer spot it will..
+
+@Simeonpitts916
+Simeonpitts916 committed 2 days ago
+ 
+I have added a headline to my html it will..
+
+@Simeonpitts916
+Simeonpitts916 committed 2 days ago
+ 
+i have added color and style to my work it will..
+
+@Simeonpitts916
+Simeonpitts916 committed 2 days ago
+ 
+A project was added to my list of questions it will
+
+@Simeonpitts916
+Simeonpitts916 committed 2 days ago
+ 
+I have added to the feature,framework,Learn questions it will..
+
+@Simeonpitts916
+Simeonpitts916 committed 2 days ago
+ 
+test it will
+
+@Simeonpitts916
+Simeonpitts916 committed 2 days ago
+
+## known bugs
+## Copyright, date, and licens information
+Copyright simeon pitts 1/28/2023 licens: vs code, css
--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
+
+let learn = document.getElementById("learn");
+let cross = document.getElementById("cross-platfom");
+let angular = document.getElementById("angular");
+let net = document.getElementById("net");
+let online = document.getElementById("online");
+let inschool = document.getElementById("inschool");
+let gaming = document.getElementById("gaming");
+let web = document.getElementById("web");
+let mcdonald = document.getElementById("Mcdonald");
+let Burger = document.getElementById("Burger");
+
+
 window.onload = function() {
     const form = document.querySelector ("form");
+    const newLocal = this;
     form.onsubmit = function(event) {
         event.preventDefault();
-        //question 1
-        let learn = document.getElementById("learn");
-        let cross = document.getElementById("cross-platfom");
-        //question 2
-        let angular = document.getElementById("angular");
-        let net = document.getElementById("net");
-        //question 3
-        let online = document.getElementById("online");
-        let inschool = document.getElementById("inschool");
-        //question 4
-        let gaming = document.getElementById("gaming");
-        let web = document.getElementById("web");
-        //question 5
-        let Mcdonald = document.getElementById("Mcdonald");
-        let Burger = document.getElementById("Burger");
-
-        if () {
-            
-        }
     }
 }
+
+let results = getElementById("results")
+results = setAttribute("class", "hidden")
+
+let answer = getElementById("answer")
+answer = setAttribute("class", "hidden")
